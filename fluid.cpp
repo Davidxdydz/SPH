a58d745206d6a783b382a35dea8e97c58d006b7f
David Berger <david.p.berger@gmx.de>
2023-10-17 22:09:36 +0200

    Add untested infinite hashgrid

--- fluid.cpp
+++ fluid.cpp
@@ -1,6 +1,8 @@
 #include "fluid.h"
 #include <iostream>
+#include <algorithm>
 #include <glm/gtc/random.hpp>
+using namespace glm;
 
 Fluid::Fluid(GLuint instancingShaderID) : renderer(instancingShaderID, transforms, colors, 2)
 {
@@ -200,4 +202,84 @@ void Fluid::draw()
         colors[i] = vec3(normalized, 1 - normalized, 0);
     }
     renderer.draw();
+}
+
+Grid::Grid(float size, int tableSize, std::vector<Transform> &transforms) : size(size), transforms(transforms), tableSize(tableSize)
+{
+    hashs.assign(transforms.size(), 0);
+    sortedHashIndices.assign(transforms.size(), 0);
+    startIndices.assign(tableSize, -1);
+    update();
+}
+
+void Grid::update()
+{
+    for (int i = 0; i < transforms.size(); i++)
+    {
+        hashs[i] = hash(transforms[i].position);
+        sortedHashIndices[i] = i;
+    }
+    std::sort(sortedHashIndices.begin(), sortedHashIndices.end(), [&](int a, int b)
+              { return hashs[a] < hashs[b]; });
+    int lastHash = -1;
+    for (int i = 0; i < transforms.size(); i++)
+    {
+        if (hashs[sortedHashIndices[i]] != lastHash)
+        {
+            startIndices[hashs[sortedHashIndices[i]]] = i;
+            lastHash = hashs[sortedHashIndices[i]];
+        }
+    }
+}
+
+ivec3 Grid::cellIds(vec3 pos)
+{
+    return ivec3(pos / size);
+}
+
+int Grid::hash(vec3 pos)
+{
+    ivec3 ids = cellIds(pos);
+    return abs((ids.x * 92837111) ^ (ids.y * 689287499) ^ (ids.z * 283923481)) % tableSize;
+}
+
+std::vector<int> Grid::getCell(vec3 pos)
+{
+    int index = hash(pos);
+    int start = startIndices[index];
+    std::vector<int> result;
+    if (start == -1)
+        return result;
+    for (int i = start; i < sortedHashIndices.size(); i++)
+    {
+        if (hashs[sortedHashIndices[i]] != index)
+            break;
+        result.push_back(sortedHashIndices[i]);
+    }
+    return result;
+}
+
+std::vector<int> Grid::getNeighbors(vec3 pos)
+{
+    std::vector<int> result;
+    for (int x = -1; x < 2; x++)
+    {
+        for (int y = -1; y < 2; y++)
+        {
+            for (int z = -1; z < 2; z++)
+            {
+                int cellHash = hash(pos + vec3(x, y, z) * size);
+                int start = startIndices[cellHash];
+                if (start == -1)
+                    return result;
+                for (int i = start; i < sortedHashIndices.size(); i++)
+                {
+                    if (hashs[sortedHashIndices[i]] != cellHash)
+                        break;
+                    result.push_back(sortedHashIndices[i]);
+                }
+            }
+        }
+    }
+    return result;
 }
--- fluid.h
+++ fluid.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <vector>
+
+#include <glm/glm.hpp>
+
 #include "RenderObject.h"
 
 class Fluid
@@ -23,13 +26,35 @@ private:
     float damping;
     float m;
     std::vector<Transform> transforms;
-    std::vector<vec3> colors;
-    std::vector<vec3> vs;
+    std::vector<glm::vec3> colors;
+    std::vector<glm::vec3> vs;
     std::vector<float> densities;
     std::vector<float> pressures;
-    std::vector<vec3> as;
+    std::vector<glm::vec3> as;
     SpheresRenderer renderer;
     float W(float r, float h);
     float dW(float r, float h);
     void applyBoundaries();
+};
+
+class Grid
+{
+public:
+    float size;
+    int tableSize;
+    Grid(float size, int tableSize, std::vector<Transform> &transforms);
+    void update();
+    /*
+    Get indices of all particles in the same cell and the 26 surrounding cells
+    */
+    std::vector<int> getNeighbors(glm::vec3 pos);
+    std::vector<int> getCell(glm::vec3 pos);
+
+private:
+    std::vector<Transform> &transforms;
+    std::vector<int> hashs;
+    std::vector<int> sortedHashIndices;
+    std::vector<int> startIndices;
+    int hash(glm::vec3 pos);
+    glm::ivec3 cellIds(glm::vec3 pos);
 };
