c7071b672218a57bda0f4fe9624fa3c68a3bb128
David Berger <david.p.berger@gmx.de>
2023-10-19 16:33:23 +0200

    Formating

--- shapes.h
+++ shapes.h
@@ -54,7 +54,7 @@ static const GLfloat triangleVertices[] = {
 
 struct Triangle
 {
-  GLuint vertex[3];
+    GLuint vertex[3];
 };
 
 using TriangleList = std::vector<Triangle>;
@@ -64,17 +64,17 @@ using IndexedMesh = std::pair<VertexList, TriangleList>;
 
 namespace icosahedron
 {
-  const float X = .525731112119133606f;
-  const float Z = .850650808352039932f;
-  const float N = 0.f;
+    const float X = .525731112119133606f;
+    const float Z = .850650808352039932f;
+    const float N = 0.f;
 
-  static const VertexList vertices =
-      {
-          {-X, N, Z}, {X, N, Z}, {-X, N, -Z}, {X, N, -Z}, {N, Z, X}, {N, Z, -X}, {N, -Z, X}, {N, -Z, -X}, {Z, X, N}, {-Z, X, N}, {Z, -X, N}, {-Z, -X, N}};
+    static const VertexList vertices =
+        {
+            {-X, N, Z}, {X, N, Z}, {-X, N, -Z}, {X, N, -Z}, {N, Z, X}, {N, Z, -X}, {N, -Z, X}, {N, -Z, -X}, {Z, X, N}, {-Z, X, N}, {Z, -X, N}, {-Z, -X, N}};
 
-  static const TriangleList triangles =
-      {
-          {0, 4, 1}, {0, 9, 4}, {9, 5, 4}, {4, 5, 8}, {4, 8, 1}, {8, 10, 1}, {8, 3, 10}, {5, 3, 8}, {5, 2, 3}, {2, 7, 3}, {7, 10, 3}, {7, 6, 10}, {7, 11, 6}, {11, 0, 6}, {0, 1, 6}, {6, 1, 10}, {9, 0, 11}, {9, 11, 2}, {9, 2, 5}, {7, 2, 11}};
+    static const TriangleList triangles =
+        {
+            {0, 4, 1}, {0, 9, 4}, {9, 5, 4}, {4, 5, 8}, {4, 8, 1}, {8, 10, 1}, {8, 3, 10}, {5, 3, 8}, {5, 2, 3}, {2, 7, 3}, {7, 10, 3}, {7, 6, 10}, {7, 11, 6}, {11, 0, 6}, {0, 1, 6}, {6, 1, 10}, {9, 0, 11}, {9, 11, 2}, {9, 2, 5}, {7, 2, 11}};
 }
 
 GLuint vertex_for_edge(Lookup &lookup,
